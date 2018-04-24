9007a38efc2a5feaefd410d52cda396ef489b611
Harry <harry@h313.info>
2018-04-24 09:33:38 -0400

    add a interval to notify every 5 minutes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const router = require('./route');
-const webPush = require('web-push');
+
+const notify = require('./notification');
+const redis = require('./db/redis');
+const moment = require('moment');
 
 const app = new Koa();
 
 app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+setInterval(function() {
+    const keys = await redis.keys();
+    keys.forEach(function(element) {
+        const time = await redis.get(element);
+        if(moment.duration(now.diff(time)).asHours() >= 2) {
+            notify(element);
+        }
+    });
+}, 300000);
 
 app.listen(3000);
