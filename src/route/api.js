035b3bbd8e2324041b7d6314603750ab03be3f7a
Harry <harry@h313.info>
2018-04-24 09:33:04 -0400

    move getting vapid key from api to homepage

--- src/route/api.js
+++ src/route/api.js
@@ -1,17 +1,12 @@
 const Router = require('koa-router');
-const notify = require('../notification');
 const redis = require('../db/redis');
 const moment = require('moment');
 
 const router = new Router();
 
-router.get('/key', async (ctx) => {
-    ctx.body = process.env.VAPID_PUBLIC_KEY;
-});
-
 router.post('/register', async (ctx) => {
-    await redis.set(ctx.request.body, moment());
-    ctx.status = 201;
+    await redis.set(req.body.subscription, moment());
+    ctx.body = JSON.stringify({success: 1});
 });
 
 module.exports = router;
--- src/route/homepage.js
+++ src/route/homepage.js
@@ -6,4 +6,8 @@ router.get('/test', async (ctx) => {
     ctx.body = 'working';
 });
 
+router.get('/vapidPublicKey', async (ctx) => {
+    ctx.body = process.env.VAPID_PUBLIC_KEY;
+});
+
 module.exports = router;
