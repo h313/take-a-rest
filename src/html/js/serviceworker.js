93d50d0b03d91b99bd3aaf6481a809c57dc728d7
Harry <harry@h313.info>
2018-04-24 09:33:19 -0400

    add init.js to frontend

--- src/html/js/serviceworker.js
+++ src/html/js/serviceworker.js
@@ -1,7 +1,7 @@
 self.addEventListener('push', function(event) {
     event.waitUntil(
-        self.registration.showNotification('ServiceWorker Cookbook', {
-            body: 'Take a rest!',
+        self.registration.showNotification('Take a rest!', {
+            body: 'Drink some water, or maybe go outside?',
             lang: 'en',
             vibrate: [50],
         })
